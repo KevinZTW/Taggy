4130ca8c098c45eb61b004b76c3cafc297c80721
Kevin <kai821104@gmail.com>
2020-11-28 16:53:44 +0800

    revised greenlock setting

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 greenlock
   .init({
     packageRoot: __dirname,
+    configDir: "./greenlock.d",
     maintainerEmail: "kevin.zhang.tw@gmail.com", // The email address of the ACME user / hosting provider
     agreeTos: true, // You must accept the ToS as the host which handles the certs
-    configDir: "./cert/", // Writable directory where certs will be saved
     // Using your express app:
     // simply export it as-is, then include it here
     store: store,
