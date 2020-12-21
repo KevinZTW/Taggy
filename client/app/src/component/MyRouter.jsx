1f391c0d2cb88dc877be9b3d0577b7e5068935c7
Kevin <kai821104@gmail.com>
2020-12-21 12:10:53 +0800

    revise auth routing to exclude landing and sign in page

--- client/app/src/component/MyRouter.jsx
+++ client/app/src/component/MyRouter.jsx
@@ -4,10 +4,15 @@ import { useEffect } from "react";
 import { auth } from "../firebase.js";
 import { useDispatch, useSelector } from "react-redux";
 import { SETMEMBER } from "../redux/actions";
-import { BrowserRouter as Router, useHistory } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  useHistory,
+  useLocation,
+} from "react-router-dom";
 export default function MyRouter() {
   let dispatch = useDispatch();
-
+  let location = useLocation();
+  console.log(location.pathname);
   const history = useHistory();
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -17,7 +22,8 @@ export default function MyRouter() {
         dispatch(SETMEMBER(user.uid, user.displayName, user.email));
         // history.push("/home");
       } else {
-        history.push("/signup");
+        if (location.pathname !== "/" || location.pathname !== "/signin")
+          history.push("/signup");
       }
     });
   }, [dispatch, history]);
