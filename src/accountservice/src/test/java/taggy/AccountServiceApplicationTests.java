90203ca1d90f29a5a2080103fe77dfd33f9bafae
Kevin Zhang <kevinzhang@KevindeMacBook-Pro.local>
2023-07-25 20:25:07 +0800

    [Account Srv] feat: init account service

---
+++ src/accountservice/src/test/java/taggy/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package taggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
